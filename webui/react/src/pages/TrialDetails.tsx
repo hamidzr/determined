8e676011d8a31d63a912799134aa83633242b29a
Hamid Zare <hamid@determined.ai>
2020-05-19 11:31:09 -0700

    bring in data from a sample trial

--- webui/react/src/pages/TrialDetails.tsx
+++ webui/react/src/pages/TrialDetails.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import trialResponse from 'assets/sample-trial.json';
+
 const TrialDetails: React.FC = () => {
+  console.log(trialResponse);
+
   return <div>td page</div>;
 };
 
