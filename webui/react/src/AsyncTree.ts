db4b5afd9766ed25aba88fe021b0c08e411dc100
Hamid Zare <hamid@determined.ai>
2020-05-25 19:19:18 -0700

    add tree Children type

--- webui/react/src/AsyncTree.ts
+++ webui/react/src/AsyncTree.ts
@@ -3,16 +3,17 @@ import { isAsyncFunction } from 'utils/data';
 interface BaseNodeProps {
   title: string; // should work with the separator. no space?
 }
+export type Children = TreeNode[]
 export type TreePath = TreeNode[]
 export type TreeNode = LeafNode | NLNode;
-export type TreeGenerator = (arg?: NLNode) => TreeNode[] | Promise<TreeNode[]>
+export type ComputedChildren = (arg?: NLNode) => Children | Promise<Children>
 
 export interface LeafNode extends BaseNodeProps {
   onAction: (arg: LeafNode) => void; // with potential response. could be shown
 }
 
 export interface NLNode extends BaseNodeProps {
-  options: TreeNode[] | TreeGenerator; // leaf nodes have no children
+  options: Children | ComputedChildren; // leaf nodes have no children
 }
 
 export const isLeafNode = (node: any): node is LeafNode =>
@@ -22,13 +23,13 @@ export const isNLNode = (node: any): node is NLNode =>
 export const isTreeNode = (node: any): node is TreeNode =>
   node.title !== undefined && (isLeafNode(node) || isNLNode(node));
 
-export const getNodeChildren = async (node: NLNode): Promise<TreeNode[]> => {
-  let children: TreeNode[] = [];
+export const getNodeChildren = async (node: NLNode): Promise<Children> => {
+  let children: Children = [];
   if (typeof node.options === 'function') {
     if (isAsyncFunction(node.options)) {
       children = await node.options(node);
     } else {
-      children = node.options(node) as TreeNode[];
+      children = node.options(node) as Children;
     }
   } else {
     children = node.options;
@@ -41,7 +42,7 @@ export const traverseTree = async (address: string[], startNode: NLNode): Promis
   const path: TreePath = [ curNode ];
   let i = 0;
   while(isNLNode(curNode) && i<address.length) {
-    const children: TreeNode[] = await getNodeChildren(curNode);
+    const children: Children = await getNodeChildren(curNode);
     const rv = children.find(n => n.title === address[i]);
     if (rv === undefined) break;
     curNode = rv;
