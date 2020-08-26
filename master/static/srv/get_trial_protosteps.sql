5488e3f8d92662eb1a061858afa7188d1c075096
Hamid Zare <hamid@determined.ai>
2020-08-26 16:17:56 -0700

    add partial validations

--- master/static/srv/get_trial_protosteps.sql
+++ master/static/srv/get_trial_protosteps.sql
@@ -6,7 +6,8 @@ SELECT s.id,
   s.start_time,
   s.end_time,
   s.num_batches,
-  s.prior_batches_processed -- (
+  s.prior_batches_processed,
+  -- (
   --   SELECT row_to_json(r3)
   --   FROM (
   --       SELECT c.id,
@@ -23,21 +24,19 @@ SELECT s.id,
   --         AND c.step_id = s.id
   --     ) r3
   -- ) AS checkpoint,
-  -- (
-  --   SELECT row_to_json(r4)
-  --   FROM (
-  --       SELECT v.id,
-  --         v.trial_id,
-  --         v.step_id,
-  --         v.state,
-  --         v.start_time,
-  --         v.end_time,
-  --         v.metrics
-  --       FROM validations v
-  --       WHERE v.trial_id = t.id
-  --         AND v.step_id = s.id
-  --     ) r4
-  -- ) AS validation
+  (
+    SELECT row_to_json(r4)
+    FROM (
+        SELECT v.id,
+          'STEP_STATE_' || v.state as state,
+          v.start_time,
+          v.end_time,
+          (v.metrics->'num_inputs') AS num_inputs
+        FROM validations v
+        WHERE v.trial_id = t.id
+          AND v.step_id = s.id
+      ) r4
+  ) AS validation
 FROM steps s
   INNER JOIN trials t ON (
     s.trial_id = t.id
