a119e250722e1ac5748ef02854ee87678a2be90a
Hamid Zare <hamid@determined.ai>
2020-08-31 16:23:52 -0700

    Squashed commit of the base trial apis
    commit 0a3a31366cdd537d866aea0e2e2d17c43fa05eb6
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Mon Aug 31 15:04:22 2020 -0700
    
        add missing trailing dot
    
    commit 064d0b88848b6a545132773cfdf552f37f8873eb
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Mon Aug 31 14:55:03 2020 -0700
    
        initialize trial response
    
    commit 75dfcff4f09aeda0a4b9ed5d4af822a3b4af2d39
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Mon Aug 31 14:36:04 2020 -0700
    
        wrap returned errors
    
    commit 50ba9ad6a0a602f17f9713d06ab72d7fa1ad2381
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Mon Aug 31 14:29:07 2020 -0700
    
        add total_batches_processed to single get trial endpoint.
    
        update the sql inentation to 4 spaces
    
    commit 6b7a56e6f43decfd8c0086e2e7d3e0373176faf4
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Fri Aug 28 11:02:50 2020 -0700
    
        rename to total_batches_processed. add end field
    
    commit e1c66c3d9dcc8431b04800975898974a8da44613
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Fri Aug 28 13:33:36 2020 -0700
    
        fix a filter logic. rename proto sql file
    
    commit db94f142631eca6741f6bc7270709a4a98b1918a
    Author: Hamid Zare <dellydela@gmail.com>
    Date:   Fri Aug 28 10:58:12 2020 -0700
    
        Apply suggestions from code review
    
        Co-authored-by: Bradley Laney <bradlaney@determined.ai>
    
    commit 72b3c5308443bee9f565fb98b838c3087da901ce
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Thu Aug 27 17:32:07 2020 -0700
    
        add missing comments
    
    commit 088f41927098c21bbb4d83313dbcceb2cb5e6ce0
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Thu Aug 27 17:05:06 2020 -0700
    
        rename id to trial_id
    
    commit 642883137263b4929486e9c03fc237ca4e77b965
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Thu Aug 27 16:59:28 2020 -0700
    
        add 404 support
    
    commit 8493deafa4df7ab92cea0f5cd10143b8356b549f
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Thu Aug 27 16:50:16 2020 -0700
    
        remove extra changes
    
    commit 2305f5ce4fef906d908fa54e808a6c7d5563c8a7
    Author: Hamid Zare <hamid@determined.ai>
    Date:   Fri Aug 21 11:23:45 2020 -0700
    
        add base trial endpoints

--- master/internal/api_trials.go
+++ master/internal/api_trials.go
@@ -15,7 +15,6 @@ import (
 	"github.com/determined-ai/determined/master/pkg/model"
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
 	"github.com/determined-ai/determined/proto/pkg/checkpointv1"
-	"github.com/determined-ai/determined/proto/pkg/stepv1"
 	"github.com/determined-ai/determined/proto/pkg/trialv1"
 )
 
@@ -161,114 +160,142 @@ func (a *apiServer) KillTrial(
 	return &resp, err
 }
 
-// func trialToProcessedLength() {
+// func (a *apiServer) GetExperimentTrials(
+// 	_ context.Context, req *apiv1.GetExperimentTrialsRequest) (*apiv1.GetExperimentTrialsResponse, error) {
+// 	resp := &apiv1.GetExperimentTrialsResponse{}
 
-// 	// addr := actor.Addr("trials", req.Id).String()
-// 	// switch err = a.actorRequest(addr, req, &resp); {
+// 	switch err := a.m.db.QueryProto("get_trials_for_experiment", &resp.Trials, req.ExperimentId); {
+// 	case err == db.ErrNotFound:
+// 		return nil, status.Errorf(codes.NotFound, "experiment %d not found:", req.ExperimentId)
+// 	case err != nil:
+// 		return nil, err
+// 	}
+// 	a.filter(&resp.Trials, func(i int) bool {
+// 		v := resp.Trials[i]
+// 		eliminate := false
+
+// 		if len(req.States) != 0 && !eliminate {
+// 			eliminate = true
+// 			for _, state := range req.States {
+// 				if state == v.State {
+// 					eliminate = false
+// 					break
+// 				}
+// 			}
+// 		}
+
+// 		return !eliminate
+// 	})
+
+// 	a.sort(resp.Trials, req.OrderBy, req.SortBy, apiv1.GetExperimentTrialsRequest_SORT_BY_ID)
+// 	if err := a.paginate(&resp.Pagination, &resp.Trials, req.Offset, req.Limit); err != nil {
+// 		return nil, err
+// 	}
+
+// expConfig, err := a.m.db.ExperimentConfig(int(req.ExperimentId))
+// if err != nil {
+// 	return nil, err
+// }
+
+// type valCheckpoint struct {
+// 	BestValidation   float64 `json:"best_validation"`
+// 	LatestValidation float64 `json:"latest_validation"`
+// 	BestCheckpoint   string  `json:"best_checkpoint"`
+// }
+
+// for _, trial := range resp.Trials {
+// 	// TODO consider missing (eg terminal) trials
+// 	trialAddr := actor.Addr("trials", trial.Id)
+// 	var tbp int
+// 	// OPT do in parallel?
+
+// 	resp := a.m.system.AskAt(trialAddr, trialProgress{})
+// 	switch {
+// 	case resp.Empty():
+// 		// status.Errorf(codes.NotFound, "/api/v1%s not found", addr)
+// 		fmt.Println("no active actor")
+// 		// FIXME how do I calculate this
+// 	case resp.Error() != nil:
+// 		return nil, resp.Error()
+// 	default:
+// 		reflect.ValueOf(tbp).Elem().Set(reflect.ValueOf(resp.Get()))
+// 		trial.ProcessedLength = &trialv1.Length{
+// 			Value: int32(tbp),
+// 			Unit:  trialv1.LengthUnit_LENGTH_UNIT_BATCHES,
+// 		}
+
+// 	}
+
+// var stats valCheckpoint
+// res, err := a.m.db.RawQuery("get_trial_stats", req.ExperimentId, trial.Id)
+// if err != nil {
+// 	return nil, err
+// }
+// fmt.Println(res)
+// if err = json.Unmarshal(res, &stats); err != nil {
+// 	return nil, err
+// }
+
+// // TODO populate the whole checkpoint
+// trial.BestCheckpoint = &checkpointv1.Checkpoint{Uuid: stats.BestCheckpoint}
+// trial.BestValidation = stats.BestValidation
+// trial.LatestValidation = stats.LatestValidation
+// }
+
+// 	return resp, nil
 // }
 
 func (a *apiServer) GetExperimentTrials(
-	_ context.Context, req *apiv1.GetExperimentTrialsRequest) (*apiv1.GetExperimentTrialsResponse, error) {
+	_ context.Context, req *apiv1.GetExperimentTrialsRequest,
+) (*apiv1.GetExperimentTrialsResponse, error) {
 	resp := &apiv1.GetExperimentTrialsResponse{}
 
-	switch err := a.m.db.QueryProto("get_trials_for_experiment", &resp.Trials, req.ExperimentId); {
+	switch err := a.m.db.QueryProto(
+		"proto_get_trials_for_experiment",
+		&resp.Trials,
+		req.ExperimentId,
+	); {
 	case err == db.ErrNotFound:
 		return nil, status.Errorf(codes.NotFound, "experiment %d not found:", req.ExperimentId)
 	case err != nil:
-		return nil, err
+		return nil, errors.Wrapf(err, "failed to get trials for experiment %d", req.ExperimentId)
 	}
 	a.filter(&resp.Trials, func(i int) bool {
 		v := resp.Trials[i]
-		eliminate := false
+		if len(req.States) == 0 {
+			return true
+		}
 
-		if len(req.States) != 0 && !eliminate {
-			eliminate = true
-			for _, state := range req.States {
-				if state == v.State {
-					eliminate = false
-					break
-				}
+		for _, state := range req.States {
+			if state == v.State {
+				return true
 			}
 		}
 
-		return !eliminate
+		return false
 	})
 
 	a.sort(resp.Trials, req.OrderBy, req.SortBy, apiv1.GetExperimentTrialsRequest_SORT_BY_ID)
-	if err := a.paginate(&resp.Pagination, &resp.Trials, req.Offset, req.Limit); err != nil {
-		return nil, err
-	}
-
-	// expConfig, err := a.m.db.ExperimentConfig(int(req.ExperimentId))
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// type valCheckpoint struct {
-	// 	BestValidation   float64 `json:"best_validation"`
-	// 	LatestValidation float64 `json:"latest_validation"`
-	// 	BestCheckpoint   string  `json:"best_checkpoint"`
-	// }
-
-	// for _, trial := range resp.Trials {
-	// 	// TODO consider missing (eg terminal) trials
-	// 	trialAddr := actor.Addr("trials", trial.Id)
-	// 	var tbp int
-	// 	// OPT do in parallel?
-
-	// 	resp := a.m.system.AskAt(trialAddr, trialProgress{})
-	// 	switch {
-	// 	case resp.Empty():
-	// 		// status.Errorf(codes.NotFound, "/api/v1%s not found", addr)
-	// 		fmt.Println("no active actor")
-	// 		// FIXME how do I calculate this
-	// 	case resp.Error() != nil:
-	// 		return nil, resp.Error()
-	// 	default:
-	// 		reflect.ValueOf(tbp).Elem().Set(reflect.ValueOf(resp.Get()))
-	// 		trial.ProcessedLength = &trialv1.Length{
-	// 			Value: int32(tbp),
-	// 			Unit:  trialv1.LengthUnit_LENGTH_UNIT_BATCHES,
-	// 		}
-
-	// 	}
-
-	// var stats valCheckpoint
-	// res, err := a.m.db.RawQuery("get_trial_stats", req.ExperimentId, trial.Id)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// fmt.Println(res)
-	// if err = json.Unmarshal(res, &stats); err != nil {
-	// 	return nil, err
-	// }
-
-	// // TODO populate the whole checkpoint
-	// trial.BestCheckpoint = &checkpointv1.Checkpoint{Uuid: stats.BestCheckpoint}
-	// trial.BestValidation = stats.BestValidation
-	// trial.LatestValidation = stats.LatestValidation
-	// }
-
-	return resp, nil
+	return resp, a.paginate(&resp.Pagination, &resp.Trials, req.Offset, req.Limit)
 }
 
-// this could basically be a special case of getTrials assuming we support include_steps query param
 func (a *apiServer) GetTrial(_ context.Context, req *apiv1.GetTrialRequest) (
 	*apiv1.GetTrialResponse, error,
 ) {
-	var protoTrial trialv1.Trial
-	var response apiv1.GetTrialResponse
-	if err := a.m.db.QueryProto("get_prototrial", &protoTrial, req.Id); err != nil {
-		return nil, err
+	resp := &apiv1.GetTrialResponse{Trial: &trialv1.Trial{}}
+	switch err := a.m.db.QueryProto("proto_get_trial", resp.Trial, req.TrialId); {
+	case err == db.ErrNotFound:
+		return nil, status.Errorf(codes.NotFound, "trial %d not found:", req.TrialId)
+	case err != nil:
+		return nil, errors.Wrapf(err, "failed to get trial %d", req.TrialId)
 	}
-	response.Trial = &protoTrial
-	if req.IncludeSteps {
-		var protoSteps []*stepv1.Step
-		// OPT merge with first query for better performance.
-		if err := a.m.db.QueryProto("get_trial_protosteps", &protoSteps); err != nil {
-			return nil, err
-		}
-		response.Steps = protoSteps
-	}
-	return &response, nil
+	// if req.IncludeSteps {
+	// 	var protoSteps []*stepv1.Step
+	// 	// OPT merge with first query for better performance.
+	// 	if err := a.m.db.QueryProto("get_trial_protosteps", &protoSteps); err != nil {
+	// 		return nil, err
+	// 	}
+	// 	resp.Steps = protoSteps
+	// }
+	return resp, nil
 }
